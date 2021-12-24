f7f765890622e631e3abbb1cbc5efc9ca9a08586
biswajit312 <biswajitmishra312@gmail.com>
2021-12-24 15:51:20 +0530

    Adding first comment

--- facebook-clone-master/js/index.js
+++ facebook-clone-master/js/index.js
@@ -27,7 +27,7 @@ $(document).ready(function () {
     // Go to the next item
     $(".nxtBtn").click(function () {
         owl.trigger("next.owl.carousel");
-        // Change are you sure?
+        // The comment has been updated
     });
 
 
